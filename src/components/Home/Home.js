358b85d354f007aa1666f5e0b845372ceb6a1d9d
Vladyslav <kytsevolov@gmail.com>
2020-11-14 15:30:36 -0700

    added path to the board

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,33 @@
-import React from "react";
+import React from 'react';
 import './Home.scss';
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        }
-    };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <div className='homepage'>
-                <div className="home-bg">
-                </div>
-                <div className="home-title">
-                    <div>
-                        <h1>
-                            <span className="title-part ch1">Q</span>
-                            <span className="title-part ch2">U</span>
-                            <span className="title-part ch3">I</span>
-                            <span className="title-part ch4">Z</span>
-                        </h1>
-                        <p class="home-subtitle">Quiz Subtitle</p>
-                        <a href="/">
-                            <p>Start Quiz</p>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='homepage'>
+        <div className='home-bg'></div>
+        <div className='home-title'>
+          <div>
+            <h1>
+              <span className='title-part ch1'>Q</span>
+              <span className='title-part ch2'>U</span>
+              <span className='title-part ch3'>I</span>
+              <span className='title-part ch4'>Z</span>
+            </h1>
+            <p className='home-subtitle'>Quiz Subtitle</p>
+            <a href='/board'>
+              <p>Start Quiz</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
