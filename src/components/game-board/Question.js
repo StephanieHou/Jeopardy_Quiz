06f3f3dc60c25b4a6674e5aa5b52826cb19c0c4e
Vladyslav <kytsevolov@gmail.com>
2020-11-15 11:01:02 -0700

    get questions data and asve it on reload

--- src/components/game-board/Question.js
+++ src/components/game-board/Question.js
@@ -1,21 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import Answers from './Answers'
 import { fakeData } from '../../staticData/questionsData';
 
-const getQuestionData = (id) => {
-  return fakeData.map(({ questions }) =>
-    questions.filter((question) => question.id === id)
-  )[0][0];
-};
 
-const Question = ({ id }) => {
+
+const Question = ({ id, categoryName, setQuestionID, setCategotyName }) => {
   const [questionData, setQuestionData] = useState(null);
 
-  useEffect(() => {
-    setQuestionData(getQuestionData(id));
-  }, [id]);
+  const getQuestionData = (id) => {
+    console.log(id)
+    const {questions} = categoryName &&  fakeData
+    .filter(el => el.category === categoryName)[0]
 
-  console.log(questionData);
-  return <div>hello</div>;
+  return  id && questions.filter(question => question.id === id)[0]
+}
+
+  useEffect(() => {
+    // Get data after page reload
+    if(id) {
+      localStorage.setItem('questionId', id)
+      localStorage.setItem('categoryName', categoryName)
+    } else {
+      setQuestionID(Number(localStorage.getItem('questionId')))
+      setCategotyName(localStorage.getItem('categoryName'))
+    }
+
+      setQuestionData(getQuestionData(id));
+  }, [id, categoryName]);
+
+
+  return (
+    <div >
+      <div className="w-75 mx-auto">
+        <div className="border p-2 mt-5 text-center">{questionData && questionData.question}</div>
+          {questionData &&  <Answers answers={questionData.answers} /> }
+      </div>
+    </div>
+  );
 };
 
 export default Question;
