32f4da51d38f4866b35e4303b6708d7f9f3eefd7
Vladyslav <kytsevolov@gmail.com>
2020-11-14 18:44:08 -0700

    fake data

---
+++ src/staticData/questionsData.js
@@ -0,0 +1,214 @@
+import { v4 as id } from 'uuid';
+
+export const fakeData = [
+  {
+    category: 'Category1',
+    questions: [
+      {
+        id: 1,
+        question: 'Question1?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 2,
+        question: 'Question2?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 3,
+        question: 'Question3?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 4,
+        question: 'Question4?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 5,
+        question: 'Question5?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 6,
+        question: 'Question6?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+    ],
+  },
+  {
+    category: 'Category2',
+    questions: [
+      {
+        id: 7,
+        question: 'Question1?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 8,
+        question: 'Question2?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 9,
+        question: 'Question3?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 10,
+        question: 'Question4?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 11,
+        question: 'Question5?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 12,
+        question: 'Question6?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+    ],
+  },
+  {
+    category: 'Category3',
+    questions: [
+      {
+        id: 13,
+        question: 'Question1?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 14,
+        question: 'Question2?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 15,
+        question: 'Question3?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 16,
+        question: 'Question4?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 17,
+        question: 'Question5?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 18,
+        question: 'Question6?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+    ],
+  },
+  {
+    category: 'Category4',
+    questions: [
+      {
+        id: 19,
+        question: 'Question1?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 20,
+        question: 'Question2?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 21,
+        question: 'Question3?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 22,
+        question: 'Question4?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 23,
+        question: 'Question5?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 24,
+        question: 'Question6?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+    ],
+  },
+  {
+    category: 'Category5',
+    questions: [
+      {
+        id: 25,
+        question: 'Question1?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 26,
+        question: 'Question2?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 27,
+        question: 'Question3?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 28,
+        question: 'Question4?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 29,
+        question: 'Question5?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 30,
+        question: 'Question6?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+    ],
+  },
+  {
+    category: 'Category6',
+    questions: [
+      {
+        id: 31,
+        question: 'Question1?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 32,
+        question: 'Question2?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 33,
+        question: 'Question3?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 34,
+        question: 'Question4?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 35,
+        question: 'Question5?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+      {
+        id: 36,
+        question: 'Question6?',
+        answers: ['answer1', 'answer2', 'answer3', 'answer4'],
+      },
+    ],
+  },
+];
