d07d0914e7b3ad1997dcc06817db55e4833a3af6
Vladyslav <kytsevolov@gmail.com>
2020-11-16 08:57:55 -0700

    changed answers width

--- src/components/game-board/Answers.js
+++ src/components/game-board/Answers.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Answers = ({ answers }) => {
   return (
-    <div className='w-25 mx-auto d-flex mt-4'>
+    <div className='w-50 mx-auto d-flex mt-4 justify-content-around'>
       {answers.map((answer) => (
-        <div className='border p-3 ml-2' key={answer}>
+        <div className='border p-3' key={answer}>
           {answer}
         </div>
       ))}
