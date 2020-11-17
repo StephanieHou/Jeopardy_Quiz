acd5167e52a920903e24163e7d970d66eb76039d
Vladyslav <kytsevolov@gmail.com>
2020-11-16 19:19:40 -0700

    changes class to classname

--- src/components/GameBoard/Category/Name.js
+++ src/components/GameBoard/Category/Name.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const Name = ({ categoryName }) => {
-  return <div class="category-name-text">{categoryName}</div>;
+  return <div className='category-name-text'>{categoryName}</div>;
 };
 export default Name;
