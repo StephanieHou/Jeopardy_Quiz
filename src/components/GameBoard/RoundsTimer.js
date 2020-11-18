3a6ef0ccc95017674589c299561da8679d1048ce
Vladyslav <kytsevolov@gmail.com>
2020-11-17 18:04:39 -0700

    clean up

--- src/components/GameBoard/RoundsTimer.js
+++ src/components/GameBoard/RoundsTimer.js
@@ -1,26 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-const RoundsTimer = ({round, setRound, seconds, setSeconds}) => {
-
-  
-
-  if(round === 1) {
-    if(seconds < 0 ) {
+const RoundsTimer = ({ round, setRound, seconds, setSeconds }) => {
+  if (round === 1) {
+    if (seconds < 0) {
       setRound(2);
       setSeconds(150);
     }
-  } else if (round === 2){
-    if(seconds < 0 ) {
+  } else if (round === 2) {
+    if (seconds < 0) {
       setRound(3);
       setSeconds(1000);
     }
   }
 
-
-
   return (
     <div className='mx-auto mt-4'>
-       <h4 className={seconds > 10 ? 'text-warning' : 'text-danger'}>Round {round} : {round === 3 ? 'Wager' : seconds + 's'}</h4>
+      <h4 className={seconds > 10 ? 'text-warning' : 'text-danger'}>
+        Round {round} : {round === 3 ? 'Wager' : seconds + 's'}
+      </h4>
     </div>
   );
 };
